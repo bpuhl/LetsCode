a7000fa7d5048ef28d4552665b73944585fe1c6d
Brian Puhl <bpuhl@puhlfamily.net>
2025-05-08 14:00:06 -0700

    Created Hello.py file

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 # hello world
 print("Hello, world!")
+
+# prompt the user for their name
+name = input("What is your name? ")
+
+#print the user name to screen
+print("Hello, " + name + "!")
+# prompt the user for their age
